83400d5c059d8decbf2d89e16f97249d5e97ba5b
mazenfayez <mazenfayez56@gmail.com>
2021-10-09 00:07:21 +0200

    v2 commit

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import "../style/Project.css";
 function Project({ img, title, code }) {
   return (
     <div className="project">
-      <img src={img} alt="name" className="projImg" />
+      <img src={img} alt={title} className="projImg" />
 
       <h1 style={{ marginTop: "20px" }}>{title}</h1>
       <a href={code} style={{ width: "90%" }} target="_blank" rel="noreferrer">
