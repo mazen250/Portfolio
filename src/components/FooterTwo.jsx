dc75a76d13dd4a99f934db27fca9203e83585b9a
mazen250 <mazenfayez56@example.com>
2022-09-05 19:20:35 +0200

    project

--- src/components/FooterTwo.jsx
+++ src/components/FooterTwo.jsx
@@ -11,23 +11,6 @@ const Footer = () => {
   }, []);
   return (
     <div className="footerContainer" id="contact">
-      {/* <div className="footer">
-        <div className="left">
-          <h1>WORKING HOURS </h1>
-          <div className="hours">
-            <p>Sunday-Thursday 10 AM - 8 PM</p>
-            <p>Friday-Saturday 12 PM - 8 PM</p>
-          </div>
-        </div>
-        <div className="right">
-          <h1>ADDRESS</h1>
-          <div className="hourss">
-            <p>SOUL GYM -Sheraton heliopolis</p>
-            <p>12 Ahmed zaki st.</p>
-          </div>
-        </div>
-      </div>
-      <p>&copy; 2022 ABDELRAHMAN ATWA</p> */}
       <div
         className="footer"
         data-aos="fade-right"
